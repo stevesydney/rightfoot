d174f94f00150f678642c723c5cd8c88c646da8d
Steve Farrugia <sfarrugia@gmail.com>
2015-08-10 10:07:55 +1000

    Initial

---
+++ app/gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+
+gulp.task('sass', function(){
+	gulp.src('./assets/sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('./assets/sass/**/*.scss', ['sass']);
+});
+
+
+gulp.task('default', function(){
+
+});
---
+++ app/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Grid</title>
+	<link rel="stylesheet" media="all" href="/assets/css/main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<div class="container">
+
+<div class="row">
+
+<div class="column">
+
+	<?php include "temporary/_content-spacer.php"; ?>
+
+</div>
+
+
+</div>
+
+</div>
+
+
+</body>
+</html>
