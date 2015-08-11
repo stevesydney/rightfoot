59922074baf6372a2ce92d9689c92e1cf949de88
Steve Farrugia <sfarrugia@gmail.com>
2015-08-11 16:26:06 +1000

    Introduced clear:left to create new rows when column span exceeds the available coloumns

--- app/index.php
+++ app/index.php
@@ -7,101 +7,83 @@
 </head>
 <body>
 
+<?php
+
+	$number = 1;
+
+?>
+
 <div class="grid-a">
 
 	<div class="column">
 
-		<h2>1</h2>
-
 		<?php include "temporary/_content-spacer.php"; ?>
 
 	</div>
 
 	<div class="column">
 
-		<h2>2</h2>
-
-		<?php include "temporary/_content-spacer.php"; ?>
+		<?php $number++; include "temporary/_content-spacer.php"; ?>
 
 	</div>
 
 	<div class="column">
 
-		<h2>3</h2>
-
-		<?php include "temporary/_content-spacer.php"; ?>
+		<?php $number++; include "temporary/_content-spacer.php"; ?>
 
 	</div>
 
 	<div class="column">
 
-		<h2>4</h2>
-
-		<?php include "temporary/_content-spacer.php"; ?>
+		<?php $number++; include "temporary/_content-spacer.php"; ?>
 
 	</div>
 
 	<div class="column">
 
-		<h2>5</h2>
-
-		<?php include "temporary/_content-spacer.php"; ?>
+		<?php $number++; include "temporary/_content-spacer.php"; ?>
 
 	</div>
 
 	<div class="column">
 
-		<h2>6</h2>
-
-		<?php include "temporary/_content-spacer.php"; ?>
+		<?php $number++; include "temporary/_content-spacer.php"; ?>
 
 	</div>
 
 	<div class="column">
 
-		<h2>7</h2>
-
-		<?php include "temporary/_content-spacer.php"; ?>
+		<?php $number++; include "temporary/_content-spacer.php"; ?>
 
 	</div>
 
 	<div class="column">
 
-		<h2>8</h2>
-
-		<?php include "temporary/_content-spacer.php"; ?>
+		<?php $number++; include "temporary/_content-spacer.php"; ?>
 
 	</div>
 
 	<div class="column">
 
-		<h2>9</h2>
-
-		<?php include "temporary/_content-spacer.php"; ?>
+		<?php $number++; include "temporary/_content-spacer.php"; ?>
 
 	</div>
 
 	<div class="column">
 
-		<h2>10</h2>
-
-		<?php include "temporary/_content-spacer.php"; ?>
+		<?php $number++; include "temporary/_content-spacer.php"; ?>
 
 	</div>
 
 	<div class="column">
 
-		<h2>11</h2>
-
-		<?php include "temporary/_content-spacer.php"; ?>
+		<?php $number++; include "temporary/_content-spacer.php"; ?>
 
 	</div>
 
 	<div class="column">
 
-		<h2>12</h2>
-
-		<?php include "temporary/_content-spacer.php"; ?>
+		<?php $number++; include "temporary/_content-spacer.php"; ?>
 
 	</div>
 
